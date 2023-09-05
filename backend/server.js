831683b932fcc8afcfc1ff36dd77ec27471768d2
andz89m <andzrivero89@gmail.com>
2023-09-05 15:59:37 -0800

    server changes

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,10 @@ app.use("/api/token", tokenRoutes);
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
