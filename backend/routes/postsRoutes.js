7f7bb24d762ba11a18890a5554edcceadf7f1e13
andz89m <andzrivero89@gmail.com>
2023-11-07 18:38:05 -0800

    add backend

--- backend/routes/postsRoutes.js
+++ backend/routes/postsRoutes.js
@@ -16,7 +16,7 @@ import { protect } from "../middleware/authMiddleware.js";
 import { v4 as uuidv4 } from "uuid";
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "/public/images");
+    cb(null, "backend/public/images");
   },
   filename: (req, file, cb) => {
     cb(null, getString() + "-" + uuidv4() + path.extname(file.originalname));
