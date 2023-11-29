5607bddf52eba50243295b09bfae0f32f91258f1
andz89m <andzrivero89@gmail.com>
2023-11-29 06:27:46 -0800

    add hide button

--- frontend/src/hooks/useSearchPost.js
+++ frontend/src/hooks/useSearchPost.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { parseISO, formatDistanceToNow, format } from "date-fns";
+import { VscClose } from "react-icons/vsc";
+import { FaSearch } from "react-icons/fa";
 
 const useSearchPost = ({ posts, setResults }) => {
+  const [status, setStatus] = useState(false);
+
   const timeAgo = (timestamp) => {
     let data = "";
     let date = parseISO(timestamp);
@@ -40,46 +44,61 @@ const useSearchPost = ({ posts, setResults }) => {
   return (
     <>
       <div
-        className="fixed z-50 flex flex-col  shadow-md   rounded
+        className="fixed z-50 flex justify-center gap-1 items-center   shadow-md   rounded
                  right-3   shadow-sm top-[90px] sm:top-[60px] cursor-pointer   "
       >
-        {" "}
-        <form>
-          <label
-            forhtml="default-search"
-            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        {!status && (
+          <div
+            onClick={() => setStatus((prev) => !prev)}
+            className="bg-slate-600 p-2 rounded text-white"
           >
-            Search
-          </label>
-          <div className="relative">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
+            <FaSearch />
+          </div>
+        )}
+        {status && (
+          <div className="flex justify-center gap-1 items-center ">
+            <form>
+              <label
+                forhtml="default-search"
+                className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                Search
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                  </svg>
+                </div>
+                <input
+                  type="search"
+                  id="default-search"
+                  value={query}
+                  onChange={handleInputChange}
+                  placeholder="Search posts..."
+                  className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none"
+                  required
                 />
-              </svg>
-            </div>
-            <input
-              type="search"
-              id="default-search"
-              value={query}
-              onChange={handleInputChange}
-              placeholder="Search posts..."
-              className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none"
-              required
+              </div>
+            </form>
+            <VscClose
+              onClick={() => setStatus((prev) => !prev)}
+              className="text-2xl rounded bg-slate-400 text-gray-700"
             />
           </div>
-        </form>
+        )}
       </div>
     </>
   );
--- frontend/src/pages/usersView/LandingPage.js
+++ frontend/src/pages/usersView/LandingPage.js
@@ -6,7 +6,7 @@ const LandingPage = () => {
       <div className="bg-white flex justify-center items-center h-96 flex-col">
         <div className="bg-teal-500 p-10 rounded font-bold flex flex-col items-center">
           <div className=" text-2xl text-white"> Landing Page</div>
-          <p className="text-2xl text-white"> Surigao City Event Horizons</p>
+          <p className="text-2xl text-white"> Surigao City Events Horizon</p>
         </div>
       </div>
     </>
